e651396fa9346ab5bc488f5e9e5bd9ad68eb26d7
Madara Priede <priede.madara@gmail.com>
2021-06-16 16:22:10 +0300

    Add project files.

---
+++ PatternsData/Algorythms.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PatternsData
+{
+    [Serializable]
+    public class Algorythms
+    {
+        public const int Size = 5;
+
+        private FillStatusEnum[,] _table = new FillStatusEnum[Size,Size];
+
+        public Algorythms()
+        {
+            CreateEmptyTable();
+        }
+
+        public void CreateEmptyTable()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    _table[i, j] = FillStatusEnum.BoxEmpty;
+                }
+            }
+        }
+
+        public FillStatusEnum GetBoxValue(int row, int col)
+        {
+            CheckIfFieldIsValid(row, col);
+
+            return _table[row, col];
+        }
+
+        private static void CheckIfFieldIsValid(int row, int col)
+        {
+            if (row < 0 || row > Size - 1 || col < 0 || col > Size - 1)
+            {
+                throw new ArgumentException("No such row or colum");
+            }
+        }
+
+        public void FillCheckersPattern()
+        {
+            var fillInOdds = true;
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if(j % 2 != 0 && fillInOdds)
+                    {
+                        _table[i, j] = FillStatusEnum.BoxFilled;
+                    }
+                    else if(j % 2 == 0 && !fillInOdds)
+                    {
+                        _table[i, j] = FillStatusEnum.BoxFilled;
+                    }
+                }
+
+                fillInOdds = !fillInOdds;
+            }
+        }
+
+    }
+}
---
+++ PatternsWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using PatternsData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatternsWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Algorythms PatternField { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            PatternField = new Algorythms();
+        }
+
+        public string GetSquareImage(int row,int col)
+        {
+            if(PatternField == null)
+            {
+                return string.Empty;
+            }
+
+            var Box = PatternField.GetBoxValue(row, col);
+
+            switch (Box)
+            {
+                case FillStatusEnum.BoxFilled:
+                    return "<img src=\"/Images/FillBox.png\"/>";
+                case FillStatusEnum.BoxEmpty:
+                    return string.Empty;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public void OnPostCheckers()
+        {
+            PatternField.FillCheckersPattern();
+
+            RedirectToPage();
+        }
+    }
+}
